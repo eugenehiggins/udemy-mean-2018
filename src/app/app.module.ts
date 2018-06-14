9fc850a702d76b50ad0cebe3ddeeac26d3916cbd
Gene Higgins <ehigginsiii@cnm.edu>
2018-06-13 21:25:03 -0600

    added material header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
 import {FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule, MatCardModule, MatButtonModule} from '@angular/material';
+import {MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule} from '@angular/material';
+import {HeaderComponent} from './posts/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,7 +20,8 @@ import {MatInputModule, MatCardModule, MatButtonModule} from '@angular/material'
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
